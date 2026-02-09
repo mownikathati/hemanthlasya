f6d3f792403972cdd00802ed669ac81f5bfcfcf1
mownikathati <guddantimownika@gmail.com>
2026-02-09 14:12:36 +0530

    Add files via upload
    4feb

---
+++ sorting number java.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class SortEasy {
+    public static void main(String[] args) {
+        int[] a = {5, 2, 8, 1};
+        Arrays.sort(a);
+
+        for (int n : a) {
+            System.out.print(n + " ");
+        }
+    }
+}
+
